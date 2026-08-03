8ad9a4f2b3b90ccc3c38860ea55044524ae4dc27
daniellyfidelis15-jpg <daniellyfidelis15@gmail.com>
2026-08-02 23:18:31 -0300

    Add files via upload

---
+++ abrir_sistema.bat
@@ -0,0 +1,12 @@
+@echo off
+title Sistema de Gestao de RH - G4
+echo.
+echo ======================================================
+echo    SISTEMA DE GESTAO DE RECURSOS HUMANOS (RH G4)
+echo ======================================================
+echo.
+echo  Abrindo o sistema no seu navegador...
+echo  (Nao precisa de internet e nao instala nada)
+echo.
+start index.html
+exit
